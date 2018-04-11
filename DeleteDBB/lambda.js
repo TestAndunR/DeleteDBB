4e283c17b63dfbdb65a5341184cca8a2586b2988
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-04-11 15:10:00 +0530

    3

--- DeleteDBB/lambda.js
+++ DeleteDBB/lambda.js
@@ -2,23 +2,23 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 
-	let email = event.queryStringParameters.email;
-	console.log(email);
-	ddb.delete({
-		TableName: 'contact_us',
-		Key: {
-			'email': email
-		}
-	}, function (err, data) {
-		if (err) {
-			//handle error
-			console.log("Error");
-		} else {
-			//your logic goes here
-			console.log("success");
-		}
-	});
+	// let email = event.queryStringParameters.email;
+	// console.log(email);
+	// ddb.delete({
+	// 	TableName: 'contact_us',
+	// 	Key: {
+	// 		'email': email
+	// 	}
+	// }, function (err, data) {
+	// 	if (err) {
+	// 		//handle error
+	// 		console.log("Error");
+	// 	} else {
+	// 		//your logic goes here
+	// 		console.log("success");
+	// 	}
+	// });
 
 
-	callback(null, 'Successfully executed');
+	callback(null, JSON.parse(event));
 }
